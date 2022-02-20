3fe2904507fac9a7756c6a2d83c6821144e6da1b
nick <nicolas.maclean2326@gmail.com>
2022-02-20 12:17:21 -0600

    Restructured scripts for future extraction
    Separating project specific from generic scripts. I plan on creating another repo to store these scripts in the future, so they can be used cross-project

--- Assets/Gummi/Scripts/VFX/Tween.cs
+++ Assets/Gummi/Scripts/VFX/Tween.cs
@@ -1,49 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Gummi.VFX
 {
-    public abstract class EffectBase : MonoBehaviour
-    {
-        public abstract void Activate(System.Action OnComplete);
-        public abstract void Deactivate(System.Action OnComplete);
-    }
-
-    public static class Fade
-    {
-        /// <summary>
-        /// Performs a linear animation of color over <paramref name="duration"/>. Performs <paramref name="OnComplete"/> when done.
-        /// </summary>
-        /// <param name="graphic"></param>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        /// <param name="duration"></param>
-        /// <param name="OnComplete"></param>
-        /// <returns></returns>
-        public static IEnumerator Linear(MaskableGraphic graphic, Color from, Color to, float duration, System.Action OnComplete = null)
-        {
-            // initial color
-            graphic.color = from;
-
-            // animate color
-            float elapsedTime = 0;
-            while (elapsedTime < duration)
-            {
-                graphic.color = Color.Lerp(from, to, elapsedTime / duration);
-
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-
-            // final color
-            graphic.color = to;
-            if (OnComplete != null) { OnComplete(); }
-            yield break;
-        }
-    }
-
     public static class Tween
     {
         public static class Scale
